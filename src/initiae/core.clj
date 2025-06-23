ddc903499785d245316673267ebd8b1ca822c83d
Alexander Jandt <kurelen@pm.me>
2025-06-23 17:03:11 +0200

    Test fixture

--- src/initiae/core.clj
+++ src/initiae/core.clj
@@ -1,13 +1,19 @@
 (ns initiae.core
-  (:gen-class))
+  (:gen-class)
+  (:require
+    [clojure.edn :as edn]
+    [clojure.java.io :as io]))
+
+
+(defn load-fixture
+  []
+  (with-open
+    [r (io/reader
+         (io/resource "fixtures/initia_variants.edn"))]
+    (edn/read (java.io.PushbackReader. r))))
 
 
 (defn -main
   [& args]
   (println "Welcome to Initiae!")
   (println "Command-line args:" args))
-
-
-(defn greet
-  [name]
-  (str "Hello, " name "!"))
--- test/initiae/core_test.clj
+++ test/initiae/core_test.clj
@@ -1,8 +1,13 @@
 (ns initiae.core-test
   (:require
     [clojure.test :refer [deftest is]]
-    [initiae.core :refer [greet]])
+    [initiae.core :refer [load-fixture]]))
 
-(deftest test-greet
-  (is (= "Hello, Magda!" (greet "Magda")))
-  (is (not (= "Hello, Alex!" (greet "Magda")))))
+
+(deftest test-fixture
+  (let [data (load-fixture)]
+    (is (vector? data))
+    (doseq [entry data]
+      (doseq [[_ value] entry]
+        (is (vector? value))
+        (is (every? string? value))))))
