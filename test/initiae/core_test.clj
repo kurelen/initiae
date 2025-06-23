34a718ed30612ffac5c81193196a84daedd2fa4c
Alexander Jandt <kurelen@pm.me>
2025-06-23 16:18:22 +0200

    Satisfy kondo

--- test/initiae/core_test.clj
+++ test/initiae/core_test.clj
@@ -1,7 +1,7 @@
 (ns initiae.core-test
   (:require
-    [clojure.test :refer :all]
-    [initiae.core :refer :all]))
+    [clojure.test :refer [deftest is]]
+    [initiae.core :refer [greet]))
 
 
 (deftest test-greet
