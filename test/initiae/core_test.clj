0de540091dc94fd9e3cf80c71c47a04293abfe18
Alexander Jandt <kurelen@pm.me>
2025-06-23 16:20:24 +0200

    Fix brackets

--- test/initiae/core_test.clj
+++ test/initiae/core_test.clj
@@ -1,8 +1,7 @@
 (ns initiae.core-test
   (:require
     [clojure.test :refer [deftest is]]
-    [initiae.core :refer [greet]))
-
+    [initiae.core :refer [greet]])
 
 (deftest test-greet
   (is (= "Hello, Magda!" (greet "Magda")))
