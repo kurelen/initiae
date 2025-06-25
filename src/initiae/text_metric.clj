4a24a6c148f8d97a69a801bff84397eebd28bfa0
Alexander Jandt <kurelen@pm.me>
2025-06-25 21:50:28 +0200

    Add weighted levenshtein

--- src/initiae/text_metric.clj
+++ src/initiae/text_metric.clj
@@ -1,13 +1,17 @@
 (ns initiae.text-metric
   (:import
     (info.debatty.java.stringsimilarity
+      CharacterSubstitutionInterface
+      CharacterInsDelInterface
+      Damerau
       Cosine
       Jaccard
       JaroWinkler
       Levenshtein
       LongestCommonSubsequence
       MetricLCS
-      NGram)))
+      NGram
+      WeightedLevenshtein)))
 
 
 (defn ngram-sim
@@ -73,7 +77,43 @@
 
 
 (defn levenshtein-sim
-  "Returns levenshtein distance between two string. Can be limted (int)"
-  ([s1 s2] (/ (levenshtein-dist s1 s2) (max (count s1) (count s2))))
-  ([limit s1 s2] (/ (levenshtein-dist limit s1 s2)
-                    (max (count s1) (count s2) limit))))
+  "Returns levenshtein similarity between two string. Can be limted (int)"
+  ([s1 s2] (- 1.0
+              (/ (levenshtein-dist s1 s2)
+                 (max (count s1) (count s2)))))
+  ([limit s1 s2] (- 1.0
+                    (/ (levenshtein-dist limit s1 s2)
+                       (max (count s1) (count s2) limit)))))
+
+(defn damerau-dist
+  "Returns damarau-levenshtein distance between two string. Can be limted (int)"
+  ([s1 s2] (.distance (Damerau.) s1 s2)))
+
+
+(defn damerau-sim
+  "Returns damarau-levenshtein similarity between two string"
+  [s1 s2] (- 1.0
+              (/ (damerau-dist s1 s2)
+                 (max (count s1) (count s2)))))
+
+(def ^:private default-insert-delete-cost (constantly 1.0))
+(defn- default-substitute-cost
+  [c1 c2]
+  (if (= c1 c2) 0.0 1.0))
+
+
+(defn weighted-levenshtein-dist-fn
+  "Returns weighted levenshtein distance function between two string. Options have to be supplied with keys :substitute  (binary cost fn) :insert (unary cost fn) :delete (unary cost fn) :limit (int)"
+  [opts]
+  (let [{:keys [substitute insert delete limit]
+          :or {substitute default-substitute-cost
+               insert default-insert-delete-cost
+               delete default-insert-delete-cost
+               limit Double/MAX_VALUE}} opts
+        char-sub (reify CharacterSubstitutionInterface
+              (cost [_ c1 c2] (substitute c1 c2)))
+        char-change (reify CharacterInsDelInterface
+               (insertionCost [_ c] (insert c))
+               (deletionCost [_ c] (delete c)))
+        o (WeightedLevenshtein. char-sub char-change)]
+    (fn [s1 s2] (.distance o s1 s2 limit))))
