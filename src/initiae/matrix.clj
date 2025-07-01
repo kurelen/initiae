f3386f071034b33b93db10afd523e05d2ce6ef96
Alexander Jandt <kurelen@pm.me>
2025-07-01 07:58:55 +0200

    Improve docstring

--- src/initiae/matrix.clj
+++ src/initiae/matrix.clj
@@ -2,7 +2,16 @@
 
 
 (defn gen-symmetric
-  "Creates a symmetric matrix by evaluating a bifunction f onto each ordered pair of elements of a vector v"
+  "Generates an `n x n` symmetric matrix from a vector `v` and a symmetric binary function `f`.
+  Each element A_ij of the resulting matrix is computed as (f v_i v_j), ensuring symmetry:
+  A_ij = A_ji = (f (v i) (v j)).
+
+  Arguments:
+  - f: A symmetric binary function taking two elements of `v` and returning a scalar.
+  - v: A vector of elements.
+
+  Returns:
+  - A nested vector (matrix) of size `n x n`, symmetric across the diagonal."
   [f v]
   (let [n (count v)
         ms
